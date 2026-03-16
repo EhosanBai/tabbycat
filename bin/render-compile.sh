b3decf096b06f393b5acc79276b587ab32956301
EhosanBai <ehosanul04@gmail.com>
2026-03-16 22:46:52 +0600

    Update render-compile.sh

--- bin/render-compile.sh
+++ bin/render-compile.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
+set -o pipefail
+set -o nounset
 
-echo "-----> Install dependencies"
+echo "-----> Install pipenv"
+python -m pip install --upgrade pip
 python -m pip install pipenv
-pipenv install --system --deploy --ignore-pipfile
+
+echo "-----> Install project dependencies from Pipfile"
+pipenv install --system --deploy --ignore-pipfile || {
+  echo "Pipenv install failed - trying fallback pip install from requirements.txt if exists"
+  if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+  else
+    echo "No requirements.txt found - aborting"
+    exit 1
+  fi
+}
 
 echo "-----> I'm post-compile hook"
-cd ./tabbycat/
+cd ./tabbycat/ || { echo "cd tabbycat failed"; exit 1; }
 
 echo "-----> Running database migration"
 python manage.py migrate --noinput
@@ -15,13 +27,7 @@ python manage.py migrate --noinput
 echo "-----> Running dynamic preferences checks"
 python manage.py checkpreferences
 
-# echo "-----> Running static asset compilation"
-# export NODE_OPTIONS=--max-old-space-size=192   # < NEW LINE
-# npm install -g @vue/cli-service-global
-# npm install
-# npm run build
-
 echo "-----> Running static files compilation"
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "-----> Post-compile done"
